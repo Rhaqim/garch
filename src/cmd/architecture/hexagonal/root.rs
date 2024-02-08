6cc127405786dad90c9d30f64e1b87a9ce3ca2de
Rhaqim <anusiemj@gmail.com>
2024-02-08 03:36:09 +0100

    populating generated code readme, license and ,gitignore files

--- src/cmd/architecture/hexagonal/root.rs
+++ src/cmd/architecture/hexagonal/root.rs
@@ -2,8 +2,37 @@ use crate::cmd::{architecture::hexagonal::internal_folders, FileStructure, Folde
 
 // Root Files
 const README_FILE: &str = "README.md";
+const README_CONTENT: &str = "
+ # Hexagonal Architecture Boilerplate
+    ## Introduction
+
+    This is a boilerplate for a hexagonal architecture in Go.
+
+    ## Getting Started
+
+    To get started, clone the repository and run the following command:
+
+    ```bash
+    go run cmd/main.go
+    ```
+    ## License
+    This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+    ## Acknowledgments
+    - [Hexagonal Architecture](https://en.wikipedia.org/wiki/Hexagonal_architecture_(software))
+    - [Go](https://golang.org/)";
+
 const LICENSE_FILE: &str = "LICENSE";
+const LICENSE_CONTENT: &str = "MIT License";
+
 const GITIGNORE_FILE: &str = ".gitignore";
+const GITIGNORE_CONTENT: &str = "bin
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+";
 
 // Root Folders
 const CMD_FOLDER: &str = "cmd";
@@ -13,7 +42,7 @@ const TESTDATA_FOLDER: &str = "testdata";
 
 // CMD Folder
 const MAIN_FILE: &str = "main.go";
-const CONTENT: &str = r#"package main
+const MAIN_CONTENT: &str = r#"package main
 
 import "fmt"
 
@@ -32,7 +61,7 @@ pub fn root_folders() -> Vec<FolderStructure> {
             sub_folders: None,
             files: Some(vec![FileStructure {
                 file_name: MAIN_FILE.to_string(),
-                content: CONTENT.to_string(),
+                content: MAIN_CONTENT.to_string(),
             }]),
         },
         FolderStructure {
@@ -48,7 +77,14 @@ pub fn root_folders() -> Vec<FolderStructure> {
         FolderStructure {
             folder_title: TESTDATA_FOLDER.to_string(),
             sub_folders: None,
-            files: None,
+            files: Some(
+                vec![
+                    FileStructure {
+                        file_name: "test.txt".to_string(),
+                        content: "This is a test file".to_string(),
+                    },
+                ]
+            )
         },
     ]
 }
@@ -57,15 +93,15 @@ pub fn root_files() -> Vec<FileStructure> {
     vec![
         FileStructure {
             file_name: README_FILE.to_string(),
-            content: "".to_string(),
+            content: README_CONTENT.to_string(),
         },
         FileStructure {
             file_name: LICENSE_FILE.to_string(),
-            content: "".to_string(),
+            content: LICENSE_CONTENT.to_string(),
         },
         FileStructure {
             file_name: GITIGNORE_FILE.to_string(),
-            content: "".to_string(),
+            content: GITIGNORE_CONTENT.to_string(),
         },
     ]
 }
